9b914c13d4ac769eac7b321073ba1a3cdfd744a6
Rodrigo Coyto <rcoyto@outlook.com>
2025-03-21 00:33:12 -0300

    Removed unused imports

--- src/main/java/com/rcoyto/springBookStore/books/BookRepository.java
+++ src/main/java/com/rcoyto/springBookStore/books/BookRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface BookRepository extends JpaRepository<Book, String> {
 
     Page<Book> findByDeactivationDateIsNull(Pageable pageable);
